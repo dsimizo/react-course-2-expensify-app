b95a737acf27464a712e9e096134f20c96587e51
dsimizo <dsimizo@gmail.com>
2018-05-10 22:09:21 -0300

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,50 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-  entry: './src/app.js',  // Relative path
-  output: {
-    path: path.join(__dirname, 'public'),   // Absolute path onde o arquivo será criado
-    filename: 'bundle.js' // o arquivo pode ter qualquer nome.
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,       // Se o arquivo carregado termina em .js
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/, // para suportar .css e .scss files
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map', // Para mapear o erro no arquivo original e não no bundle.js
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true    // sempre devolve o index.html
-  }
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
+
+  return {
+    entry: './src/app.js',  // Relative path
+    output: {
+      path: path.join(__dirname, 'public'),   // Absolute path onde o arquivo será criado
+      filename: 'bundle.js' // o arquivo pode ter qualquer nome.
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,       // Se o arquivo carregado termina em .js
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/, // para suportar .css e .scss files
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            }, {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
+      }]
+    },
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map', // Para mapear o erro no arquivo original e não no bundle.js
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true    // sempre devolve o index.html
+    }
+  };
 };
+
 
 
